eaeddf51330629495f78b81d962b036dc222294b
mohamed7855 <martm7726@gmail.com>
2024-06-02 19:05:18 +0300

    create dao repository to make urls

---
+++ src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.dao;
+
+import com.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "ProductCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
