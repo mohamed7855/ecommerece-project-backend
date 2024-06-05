fbd676dd845896aeb630049234286ee9d865c6ef
mohamed7855 <martm7726@gmail.com>
2024-06-05 15:53:19 +0300

    make search for product by name

--- src/main/java/com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     // http://localhost:8082/api/products/search/findByCategoryId?id=1
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    // http://localhost:8082/api/products/search/findByNameContaining?name=%...%
+    Page<Product> findByNameContaining(@Param("name") String id, Pageable pageable);
+
 }
